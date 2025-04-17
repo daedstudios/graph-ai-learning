f1213d6db681956b1b77c917c83491e1ef5954c1
Akshat Mittal <akshatmittal.0007@gmail.com>
2025-04-17 21:22:00 +0700

    import grph scene dynamically

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
 "use client";
 
-import { Canvas, useFrame } from "@react-three/fiber";
-import { TrackballControls } from "@react-three/drei";
-import GraphViz from "@/components/GraphViz";
+import { lazy, Suspense } from "react";
+import dynamic from "next/dynamic";
+
+// Dynamically import the component that uses React Three Fiber
+const Scene = dynamic(() => import("@/components/GraphScene"), {
+  ssr: false,
+  loading: () => <div>Loading 3D Scene...</div>,
+});
+
 
 export default function Page() {
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
-      <Canvas flat camera={{ position: [0, 0, 180], far: 5000 }}>
-        <TrackballControls />
-        <color attach="background" args={[0, 0, 0]} />
-        <ambientLight color={0xcccccc} intensity={Math.PI} />
-        <directionalLight intensity={0.6 * Math.PI} />
-        <GraphViz />
-      </Canvas>
+      <Scene />
     </div>
   );
 }
