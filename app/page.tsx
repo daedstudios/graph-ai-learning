896c608cbecfdbcfb1295a7b69d290a3fb9e1db9
Akshat Mittal <akshatmittal.0007@gmail.com>
2025-04-17 21:15:10 +0700

    move graph component to seperate file

--- app/page.tsx
+++ app/page.tsx
@@ -1,89 +1,8 @@
 "use client";
-import R3fForceGraph, {
-  GraphMethods,
-  NodeObject as BaseNodeObject,
-  LinkObject,
-} from "r3f-forcegraph";
 
-// Extend NodeObject type to include additional properties
-interface NodeObject extends BaseNodeObject {
-  childLinks?: LinkObject[];
-  collapsed?: boolean;
-}
-import React, { useMemo, useState, useRef, useCallback } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { TrackballControls } from "@react-three/drei";
-
-export function GraphViz() {
-  const fgRef = useRef<GraphMethods | undefined>(undefined);
-  useFrame(() => fgRef.current?.tickFrame());
-
-  const graphData = useMemo(() => genRandomTree(600, true), []);
-
-  const rootId = 0;
-
-  const nodesById = useMemo(() => {
-    const nodesById: Record<number | string, NodeObject> = Object.fromEntries(
-      graphData.nodes.map((node) => [node.id, node as NodeObject])
-    );
-
-    // link parent/children
-    graphData.nodes.forEach((node: NodeObject) => {
-      node.collapsed = node.id !== rootId;
-      node.childLinks = [];
-    });
-    graphData.links.forEach((link: LinkObject) => {
-      const sourceId =
-        typeof link.source === "object" ? link.source.id : link.source;
-      if (sourceId !== undefined && nodesById[sourceId]) {
-        nodesById[sourceId]?.childLinks?.push(link);
-      }
-    });
-
-    return nodesById;
-  }, [graphData]);
-
-  const getPrunedTree = useCallback(() => {
-    const visibleNodes = [];
-    const visibleLinks = [];
-    (function traverseTree(node = nodesById[rootId]) {
-      visibleNodes.push(node);
-      if (node.collapsed) return;
-      visibleLinks.push(...(node.childLinks || []));
-      (node.childLinks || [])
-        .map((link) =>
-          typeof link.target === "object"
-            ? link.target
-            : link.target !== undefined
-            ? nodesById[link.target]
-            : undefined
-        ) // get child node
-        .forEach(traverseTree);
-    })();
-
-    return { nodes: visibleNodes, links: visibleLinks };
-  }, [nodesById]);
-
-  const [prunedTree, setPrunedTree] = useState(getPrunedTree());
-
-  const handleNodeClick = useCallback((node: NodeObject) => {
-    node.collapsed = !node.collapsed; // toggle collapse state
-    setPrunedTree(getPrunedTree());
-  }, []);
-
-  return (
-    <R3fForceGraph
-      ref={fgRef}
-      graphData={prunedTree}
-      linkDirectionalParticles={2}
-      linkDirectionalParticleWidth={0.8}
-      nodeColor={(node) =>
-        !node.childLinks.length ? "green" : node.collapsed ? "red" : "yellow"
-      }
-      onNodeClick={handleNodeClick}
-    />
-  );
-}
+import GraphViz from "@/components/GraphViz";
 
 export default function Page() {
   return (
@@ -97,16 +16,4 @@ export default function Page() {
       </Canvas>
     </div>
   );
-}
-
-export function genRandomTree(N = 300, reverse = false) {
-  return {
-    nodes: [...Array(N).keys()].map((i) => ({ id: i })),
-    links: [...Array(N).keys()]
-      .filter((id) => id)
-      .map((id) => ({
-        [reverse ? "target" : "source"]: id,
-        [reverse ? "source" : "target"]: Math.round(Math.random() * (id - 1)),
-      })),
-  };
 }
---
+++ components/GraphViz.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import R3fForceGraph, {
+  GraphMethods,
+  NodeObject as BaseNodeObject,
+  LinkObject,
+} from "r3f-forcegraph";
+
+import React, { useMemo, useState, useRef, useCallback } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { TrackballControls } from "@react-three/drei";
+
+interface NodeObject extends BaseNodeObject {
+  childLinks?: LinkObject[];
+  collapsed?: boolean;
+}
+
+export default function GraphViz() {
+  const fgRef = useRef<GraphMethods | undefined>(undefined);
+  useFrame(() => fgRef.current?.tickFrame());
+
+  const graphData = useMemo(() => genRandomTree(600, true), []);
+
+  const rootId = 0;
+
+  const nodesById = useMemo(() => {
+    const nodesById: Record<number | string, NodeObject> = Object.fromEntries(
+      graphData.nodes.map((node) => [node.id, node as NodeObject])
+    );
+
+    // link parent/children
+    graphData.nodes.forEach((node: NodeObject) => {
+      node.collapsed = node.id !== rootId;
+      node.childLinks = [];
+    });
+    graphData.links.forEach((link: LinkObject) => {
+      const sourceId =
+        typeof link.source === "object" ? link.source.id : link.source;
+      if (sourceId !== undefined && nodesById[sourceId]) {
+        nodesById[sourceId]?.childLinks?.push(link);
+      }
+    });
+
+    return nodesById;
+  }, [graphData]);
+
+  const getPrunedTree = useCallback(() => {
+    const visibleNodes = [];
+    const visibleLinks = [];
+    (function traverseTree(node = nodesById[rootId]) {
+      visibleNodes.push(node);
+      if (node.collapsed) return;
+      visibleLinks.push(...(node.childLinks || []));
+      (node.childLinks || [])
+        .map((link) =>
+          typeof link.target === "object"
+            ? link.target
+            : link.target !== undefined
+            ? nodesById[link.target]
+            : undefined
+        ) // get child node
+        .forEach(traverseTree);
+    })();
+
+    return { nodes: visibleNodes, links: visibleLinks };
+  }, [nodesById]);
+
+  const [prunedTree, setPrunedTree] = useState(getPrunedTree());
+
+  const handleNodeClick = useCallback((node: NodeObject) => {
+    node.collapsed = !node.collapsed; // toggle collapse state
+    setPrunedTree(getPrunedTree());
+  }, []);
+
+  return (
+    <R3fForceGraph
+      ref={fgRef}
+      graphData={prunedTree}
+      linkDirectionalParticles={2}
+      linkDirectionalParticleWidth={0.8}
+      nodeColor={(node) =>
+        !node.childLinks.length ? "green" : node.collapsed ? "red" : "yellow"
+      }
+      onNodeClick={handleNodeClick}
+    />
+  );
+}
+
+function genRandomTree(N = 300, reverse = false) {
+  return {
+    nodes: [...Array(N).keys()].map((i) => ({ id: i })),
+    links: [...Array(N).keys()]
+      .filter((id) => id)
+      .map((id) => ({
+        [reverse ? "target" : "source"]: id,
+        [reverse ? "source" : "target"]: Math.round(Math.random() * (id - 1)),
+      })),
+  };
+}
