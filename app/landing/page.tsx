b842ada986bcfb93382e60fd975605dd0076f1fb
eliasheininger <eliasheininger@web.de>
2025-04-21 23:44:42 +0800

    cards on phone and cta section added and minor adjustments

--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Hero from "@/components/landing/hero";
 import Info from "@/components/landing/infoCard";
+import Cta from "@/components/landing/cta";
 export default function Landing() {
   return (
     <>
-      <Hero />
+      <div className="bg-gradient-to-b from-gray-200 to-background">
+        <Hero />
+      </div>
       <Info />
+      <Cta />
     </>
   );
 }
--- components/landing/infoCard.tsx
+++ components/landing/infoCard.tsx
@@ -5,11 +5,11 @@ import { div } from "three/tsl";
 export default function Info() {
   return (
     <>
-      <div className="flex flex-wrap gap-4 justify-center mx-auto max-w-[70rem] py-[12rem] items-start">
+      <div className="flex flex-wrap gap-12 justify-center mx-auto max-w-[70rem] py-[12rem] items-start">
         {info_data.map((item, index) => (
           <div
             key={index}
-            className="flex flex-col p-[1.5rem] bg-background border justify-between border-muted w-[21rem] rounded-[2rem] h-[31rem]"
+            className="flex flex-col p-[1.5rem] bg-background border justify-between border-muted w-[90%] md:w-[21rem] rounded-[2rem] h-[31rem]"
           >
             <div className="flex flex-row justify-between items-start">
               <div className="text-[1.5rem] leading-[125%] font-medium text-foreground w-[60%]">
@@ -20,6 +20,7 @@ export default function Info() {
                 alt="plus"
                 width={36}
                 height={36}
+                className="hover:w-[3rem] cursor-pointer"
               />
             </div>
 
@@ -28,7 +29,8 @@ export default function Info() {
               alt="visual"
               width={0}
               height={0}
-              className="w-full h-auto"
+              sizes="100vh"
+              className="w-full h-auto blur-sm hover:blur-none cursor-pointer"
             />
 
             <p className="text-[1rem] text-muted-foreground whitespace-pre-line">
