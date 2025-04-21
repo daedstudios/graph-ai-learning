919055dc4544d9c0044b4ba615ca5f71c31eba8e
eliasheininger <eliasheininger@web.de>
2025-04-21 18:03:51 +0800

    added landing page, hero section, card section and files

---
+++ app/landing/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Hero from "@/components/landing/hero";
+import Info from "@/components/landing/infoCard";
+export default function Landing() {
+  return (
+    <>
+      <Hero />
+      <Info />
+    </>
+  );
+}
