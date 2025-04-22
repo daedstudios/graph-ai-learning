7f9d4a4b9d05bb36b529e5b61c3b289061ec19c6
Akshat Mittal <93286254+drifter089@users.noreply.github.com>
2025-04-22 19:59:08 +0700

    Update steps.tsx

--- components/landing/steps.tsx
+++ components/landing/steps.tsx
@@ -4,11 +4,11 @@ import steps_data from "@/data/steps_data";
 export default function Steps() {
   return (
     <>
-      <div className="flex flex-wrap gap-12 justify-center mx-auto max-w-[70rem] pt-[0rem] pb-[6rem] md:pt-[4rem] md:pb-[12rem] items-start cursor-pointer">
+      <div className="flex flex-wrap gap-12 justify-center mx-auto max-w-[70rem] pt-[0rem] pb-[6rem] md:pt-[4rem] md:pb-[12rem] items-start">
         {steps_data.map((item, index) => (
           <div
             key={index}
-            className="flex flex-col max-w-[21rem] p-[1rem] gap-[2rem]"
+            className="flex flex-col max-w-[21rem] p-[1rem] gap-[2rem] cursor-pointer"
           >
             <div className="flex flex-row border-b items-end justify-between ">
               <h1 className="text-[3rem] "> {item.step}</h1>
