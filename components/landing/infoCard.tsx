3d35c2ca21ed7b19cf955a7f74b11b488fee9ef3
Akshat Mittal <93286254+drifter089@users.noreply.github.com>
2025-04-22 19:58:40 +0700

    Update infoCard.tsx

--- components/landing/infoCard.tsx
+++ components/landing/infoCard.tsx
@@ -22,7 +22,7 @@ export default function Info() {
 
   return (
     <>
-      <div className="flex flex-wrap gap-12 justify-center mx-auto max-w-[70rem] pt-[6rem] pb-[6rem] items-start cursor-pointer">
+      <div className="flex flex-wrap gap-12 justify-center mx-auto max-w-[70rem] pt-[6rem] pb-[6rem] items-start">
         {info_data.map((item, index) => (
           <div
             key={index}
